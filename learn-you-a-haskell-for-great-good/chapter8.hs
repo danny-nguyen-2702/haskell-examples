6bae4f67b3ddf147271c2a7ff7c2025fcec337ff
Danny Ng <danny.nguyen.2702@gmail.com>
2022-08-01 15:56:48 +0700

    Add more exercises for chapter 8 Making Our Own Types and Typeclasses

--- learn-you-a-haskell-for-great-good/chapter8.hs
+++ learn-you-a-haskell-for-great-good/chapter8.hs
@@ -1,5 +1,6 @@
 ---------------------- MAKING OUR OWN TYPES AND TYPECLASSES ----------------------
 import qualified Data.Map as Map
+import Prelude hiding (fmap)
 
 -- A typeclass is a sort of interface that defines some behavior. If a type is a part of a typeclass, that means that it supports and implements the behavior the typeclass describes.
 
@@ -82,7 +83,7 @@ data Person = Person {
 -------------------- Type contructors and Type parameters --------------------
 
 -- Type contructor is not value contructor. A value constructor can take some values parameters and then produce a new value. For instance, the Car constructor takes three values and produces a car value. In a similar manner, type constructors can take types as parameters to produce new types
-data Maybe' a = Nothing' | Just' a  
+data Maybe' a = Nothing' | Just' a
 
 -- See the above example, the a here is the type parameter. And because there's a type parameter involved, we call Maybe a type constructor. So if we pass Char as the type parameter to Maybe, we get a type of Maybe Char. 
 -- It's very important to distinguish between the type constructor and the value constructor. When declaring a data type, the part before the = is the type constructor and the constructors after it (possibly separated by |'s) are value constructors
@@ -126,8 +127,8 @@ Member {name = "Adam", code = 44}
 -}
 
 
-data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday 
-    deriving (Eq, Ord, Show, Read, Bounded, Enum) 
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+    deriving (Eq, Ord, Show, Read, Bounded, Enum)
 
 {-
 Because it's part of the Show and Read typeclasses, we can convert values of this type to and from strings.
@@ -179,12 +180,12 @@ ghci> [minBound .. maxBound] :: [Day]
 
 type Company = [Member]
 
-type PhoneNumber = String  
-type Name = String  
-type PhoneBook = [(Name,PhoneNumber)] 
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name,PhoneNumber)]
 
 -- Type synonyms can also be parameterized. If we want a type that represents an association list type but still want it to be general so it can use any type as the keys and values, we can do this:
-type AssocList k v = [(k,v)]  
+type AssocList k v = [(k,v)]
 
 
 -- Another cool data type that takes two types as its parameters is the Either a b type. This is roughly how it's defined:
@@ -207,12 +208,12 @@ type LockerMap = Map.Map LockerNumber (LockerState, Code)
 -- We're going to use an Either String Code type to represent our result, because our lookup can fail in two ways — the locker can be taken, in which case we can't tell the code or the locker number might not exist at all. If the lookup fails
 
 lockerLookup :: LockerNumber -> LockerMap -> Either String Code
-lockerLookup lockerNumber lockerMap = 
+lockerLookup lockerNumber lockerMap =
     case Map.lookup lockerNumber lockerMap of
         Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"
-        Just (state, code) -> 
-            if state Prelude.== Taken 
-            then Left $ "Locker " ++ show lockerNumber ++ " is already taken!" 
+        Just (state, code) ->
+            if state Prelude.== Taken
+            then Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
             else Right code
 
 lockers :: LockerMap
@@ -252,8 +253,8 @@ lockers = Map.fromList [
 
 
 -- We can define functions to be automatically infix by making them comprised of only special characters. We can also do the same with constructors, since they're just functions that return a data type. So in the following custom List type, we use :-: as one of our contructors.
-infixr 5 :-:  
-data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord) 
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
 -- First off, we notice a new syntactic construct, the fixity declarations. When we define functions as operators, we can use that to give them a fixity (but we don't have to). A fixity states how tightly the operator binds and whether it's left-associative or right-associative. 
 -- Now, we can write out lists in our list type like so: 3 :-: 4 :-: 5 :-: Empty
 
@@ -302,7 +303,7 @@ ourTree = foldr treeInsert EmptyTree [7, 3, 2, 9, 5, 0, 2, 7, 5, 6, 3, 4]
 -- Typeclasses have pretty much nothing to do with classes in languages like Java or Python. In those languages, classes are a blueprint from which we then create objects that contain state and can do some actions. Typeclasses are more like interfaces. We don't make data from typeclasses.
 
 -- This is how the Eq class is defined in the standard prelude:
-class Eq' a where  
+class Eq' a where
     (==) :: a -> a -> Bool  -- this is type declaration
     (/=) :: a -> a -> Bool  -- this is type declaration
     x == y = not (x Main./= y)   -- this is function body, but it's not mandatory
@@ -335,13 +336,128 @@ instance Show TrafficLight where
 
 -- But how are the Maybe or list types made as instances of typeclasses? What makes Maybe different from, say, TrafficLight is that Maybe in itself isn't a concrete type, it's a type constructor that takes one type parameter (like Char or something) to produce a concrete type (like Maybe Char)
 -- From the type declarations (inside class declarations), we see that the type variable `a` is used as a concrete type because all the types in functions have to be concrete (remember, you can't have a function of the type a -> Maybe but you can have a function of a -> Maybe a or Maybe Int -> Maybe String). That's why we can't do something like:
-instance Eq Maybe' where
-    ...
+-- instance Eq Maybe' where
+--     ...
 
 -- It would also be tedious to write instance Eq (Maybe' Int) where, instance Eq (Maybe' Char) where, etc. for every type ever. So we could write it out like so:
-instance (Eq m) => Eq (Maybe' m) where  
-    Just' x == Just' y = x Prelude.== y  
-    Nothing' == Nothing' = True  
-    _ == _ = False 
+instance (Eq m) => Eq (Maybe' m) where
+    Just' x == Just' y = x Prelude.== y
+    Nothing' == Nothing' = True
+    _ == _ = False
 -- With this instance declaration, we say this: we want all types of the form Maybe' m to be part of the Eq typeclass, but only those types where the m (so what's contained inside the Maybe') is also a part of Eq
 -- Most of the times, class constraints in class declarations are used for making a typeclass a subclass of another typeclass and class constraints in instance declarations are used to express requirements about the contents of some type. For instance, here we required the contents of the Maybe' to also be part of the Eq typeclass.
+
+
+
+
+------------------ A yes-no typeclass example ------------------
+-- In JavaScript and some other weakly typed languages, you can put almost anything inside an if expression. For example, you can do all of the following: if (0) alert("YEAH!") else alert("NO!"), if ("") alert ("YEAH!") else alert("NO!"), if (false) alert("YEAH") else alert("NO!), etc. and all of these will throw an alert of NO!
+-- Even though strictly using Bool for boolean semantics works better in Haskell, let's try and implement that JavaScript-ish behavior anyway
+
+-- The YesNo typeclass defines one function. That function takes one value of a type that can be considered to hold some concept of true-ness and tells us for sure if it's true or not. Notice that from the way we use the a in the function, a has to be a concrete type.
+class YesNo a where
+    yesno :: a -> Bool
+
+-- For numbers, we'll assume that (like in JavaScript) any number that isn't 0 is true-ish and 0 is false-ish
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+-- Empty lists (and by extensions, strings) are a no-ish value, while non-empty lists are a yes-ish value.
+-- Notice how we just put in a type parameter a in there to make the list a concrete type, even though we don't make any assumptions about the type that's contained in the list
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id -- id is just a standard library function that takes a parameter and returns the same thing
+
+instance YesNo (Maybe a) where
+    yesno Nothing = False
+    yesno _ = True
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+-- let's go play!
+-- ghci> yesno $ length []  
+-- False  
+-- ghci> yesno "haha"  
+-- True  
+-- ghci> yesno ""  
+-- False  
+-- ghci> yesno $ Just 0  
+-- True  
+-- ghci> yesno True  
+-- True  
+-- ghci> yesno EmptyTree  
+-- False  
+-- ghci> yesno []  
+-- False  
+-- ghci> yesno [0,0,0]  
+-- True  
+-- ghci> :t yesno  
+-- yesno :: (YesNo a) => a -> Bool  
+
+
+
+------------------ The Functor typeclass ------------------
+-- Now we're going to take a look at the Functor typeclass, which is basically for things that can be mapped over. You're probably thinking about lists now, since mapping over lists is such a dominant idiom in Haskell. And you're right, the list type is part of the Functor typeclass
+
+class Functor' f where
+    fmap :: (a -> b) -> f a -> f b
+
+-- Alright. We see that it defines one function, fmap. The type of fmap is interesting. In the definitions of typeclasses so far, the type variable that played the role of the type in the typeclass was a concrete type, like the a in (==) :: (Eq a) => a -> a -> Bool. But now, the f is not a concrete type, but a type constructor that takes one type parameter. A quick refresher example: Maybe Int is a concrete type, but Maybe is a type constructor that takes one type as the parameter. Anyway, we see that fmap takes a function from one type to another and a functor applied with one type and returns a functor applied with another type.
+
+-- For example, the type signature of map is map :: (a -> b) -> [a] -> [b]. It takes a function from one type to another and a list of one type and returns a list of another type. In fact, map is just a fmap that works only on lists. Here's how the list is an instance of the Functor typeclass.
+
+instance Functor' [] where
+    fmap = map
+
+-- That's it! Notice how we didn't write instance Functor [a] where, because from the Functor' class declaration, we see that the f has to be a type constructor that thats one type. [a] is already a concrete type, while [] is a type constructor that takes one type and can produce types such as [Int], [String] or even [[String]]
+
+-- What happens when we map or fmap over an empty list? Well, of course, we get an empty list. It just turns an empty list of type [a] into an empty list of type [b]
+
+
+-- Types that can act like a box can be functors. You can think of a list as a box that has an infinite amount of little compartments and they can all be empty, one can be full and the others empty or a number of them can be full. So, what else has the properties of being like a box? For one, the `Maybe a` type. In a way, it's like a box that can either hold nothing, or it can hold one item.
+
+-- When we fmap a function with a `Maybe a` type, it turns the `Maybe a` value into Maybe (function a) value. The type constructor still stay the same (which is Maybe in this case), but the value hold inside the Maybe constructor has been changed. That's what we see in the fmap declaration (fmap :: (a -> b) -> f a -> f b), the f constructor stays the same but the value inside has been changed from a to b
+
+instance Functor' Maybe where
+    -- fmap :: (a -> b) -> Maybe a -> Maybe b
+    fmap function (Just a) = Just (function a)
+    fmap function Nothing = Nothing
+
+-- Again, notice how we wrote `instance Functor Maybe where` instead of `instance Functor (Maybe m) where`. Functor wants a type constructor that takes one type and not a concrete type. Anyway, the fmap implementation is pretty simple. If it's an empty value of Nothing, then just return a Nothing. If we map over an empty box, we get an empty box. It makes sense. Just like if we map over an empty list, we get back an empty list. If it's not an empty value, but rather a single value packed up in a Just, then we apply the function on the contents of the Just.
+
+
+
+-- Another thing that can be mapped over and made an instance of Functor is our Tree a type. It can be thought of as a box in a way (holds several or no values) and the Tree type constructor takes exactly one type parameter.
+-- Mapping over an empty tree will produce an empty tree. Mapping over a non-empty tree will be a tree consisting of our function applied to the root value and its left and right sub-trees will be the previous sub-trees, only our function will be mapped over them.
+instance Functor' Tree where
+    -- fmap :: (a -> b) -> Tree a -> Tree b
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node a l r) = Node (f a) (Main.fmap f l) (Main.fmap f r)
+
+-- We can test this fmap function for our Tree in ghci using this command ghci> fmap (*2) ourTree
+
+
+
+-- Now how about Either a b? Can this be made a functor? The Functor typeclass wants a type constructor that takes only one type parameter but Either takes two. Hmmm! I know, we'll partially apply Either by feeding it only one parameter so that it has one free parameter. Here's how Either a is a functor in the standard libraries:
+instance Functor' (Either a) where
+    -- fmap :: (b -> c) -> Either a b -> Either a c
+    fmap f (Right x) = Right (f x)
+    fmap f (Left x) = Left x
+--  You can see how we made Either a an instance instead of just Either. That's because Either a is a type constructor that takes one parameter, whereas Either takes two. If fmap was specifically for Either a, the type signature would then be (b -> c) -> Either a b -> Either a c because that's the same as (b -> c) -> (Either a) b -> (Either a) c
+
+-- Maps from Data.Map can also be made a functor because they hold values (or not!). In the case of Map k v, fmap will map a function v -> v' over a map of type Map k v and return a map of type Map k v'
+instance (Ord k) => Functor' (Map.Map k) where
+    -- fmap :: (v -> v') -> Map.Map k v -> Map.Map k v'
+    -- fmap f m = Map.fromList $ [(k, f v) | (k, v) <- Map.toList m]
+    fmap f m = Map.foldrWithKey (\k v acc -> Map.insert k (f v) acc) Map.empty m
+
