cc7392ad10e8681e68f2e5968cb00d58fed78d18
Danny Ng <danny.nguyen.2702@gmail.com>
2022-08-04 17:07:20 +0700

    Add solution for find factors of a number problem

---
+++ FIMI_exercises/primeFactorsOfANumber.hs
@@ -0,0 +1,29 @@
+-- Write a function that generates factors for a given number.
+
+-- The function takes an integer on the standard input and returns a list of integers 
+-- (ObjC: array of NSNumbers representing integers). 
+-- That list contains the prime factors in numerical sequence.
+
+-- Examples
+-- 1  ==>  []
+-- 3  ==>  [3]
+-- 8  ==>  [2, 2, 2]
+-- 9  ==>  [3, 3]
+-- 12 ==>  [2, 2, 3]
+
+
+-- we don't need to use isPrime in this exercise but I just write here for future use
+isPrime :: Int -> Bool
+isPrime n
+    | n <= 1 = False
+    | n == 2 = True
+    | otherwise = null . take 1 $ [x | x <- 2:[3,5..sqrtOfN], n `mod` x == 0]
+    where sqrtOfN = floor . sqrt .fromIntegral $ n
+
+
+findFactors :: Int -> [Int]
+findFactors 1 = []
+findFactors n =  factor : findFactors (n `div` factor)
+    where factor = head [x | x <- [2..n], n `mod` x == 0]
+
+-- test = all (all isPrime . findFactors) [20000..50000]
