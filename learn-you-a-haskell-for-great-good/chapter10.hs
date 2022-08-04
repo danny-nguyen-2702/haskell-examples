a46a798fe3ccd1773e75164dc3971703381cd8bc
Danny Ng <danny.nguyen.2702@gmail.com>
2022-08-04 17:34:20 +0700

    Create file for chapter 10

---
+++ learn-you-a-haskell-for-great-good/chapter10.hs
@@ -0,0 +1,7 @@
+-------------------------------------- Functionally Solving Problems --------------------------------------
+
+
+-- Reverse Polish notation calculator
+
+-- expression written in infix manner: 10 - (4 + 3) * 2
+-- same expression written in RPN: 10 4 3 + 2 * -
