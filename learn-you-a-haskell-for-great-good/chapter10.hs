f477a2cc943764fd7b89ade91cf1b176170d6656
Danny Ng <danny.nguyen.2702@gmail.com>
2022-08-04 22:51:37 +0700

    Add exercise Reverse Polish notation calculator

--- learn-you-a-haskell-for-great-good/chapter10.hs
+++ learn-you-a-haskell-for-great-good/chapter10.hs
@@ -1,7 +1,18 @@
 -------------------------------------- Functionally Solving Problems --------------------------------------
 
-
--- Reverse Polish notation calculator
+-------------- Reverse Polish notation calculator
 
 -- expression written in infix manner: 10 - (4 + 3) * 2
 -- same expression written in RPN: 10 4 3 + 2 * -
+
+calculateRPN :: String -> Float
+calculateRPN = head . foldl foldingFunction [] . words
+    where   foldingFunction (x:y:ys) "+" = (y + x) : ys
+            foldingFunction (x:y:ys) "-" = (y - x) : ys
+            foldingFunction (x:y:ys) "*" = (y * x) : ys
+            foldingFunction (x:y:ys) "/" = (y / x) : ys
+            foldingFunction (x:y:ys) "^" = (y ** x) : ys
+            foldingFunction (x:ys) "ln" = log x : ys
+            foldingFunction xs numberStr = read numberStr : xs
+
+-------------- Heathrow to London
