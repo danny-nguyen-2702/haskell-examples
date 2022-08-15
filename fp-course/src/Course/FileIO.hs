de6f7e5a7091981e5488db80b28ac625feb063d8
Danny Ng <danny.nguyen.2702@gmail.com>
2022-08-15 23:53:58 +0700

    Add solution for FileIO exercises

--- fp-course/src/Course/FileIO.hs
+++ fp-course/src/Course/FileIO.hs
@@ -85,46 +85,54 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile fp c= do
+  putStrLn $ "============ " ++ fp
+  putStrLn c
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles = do
+  void . sequence . map (\(fp, c) -> printFile fp c) 
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile fp = do
+  content <- readFile fp
+  pure (fp, content)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles fps = do
+  foldRight (\x acc -> let fpc = getFile x in (:.) <$> fpc <*> acc) (pure Nil) fps
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run fp = do
+  content <- readFile fp 
+  let filePaths = lines content
+
+  files <- getFiles filePaths
+  
+  printFiles files
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = do
+  filePath <- getArgs
+  run $ unwords filePath
 
 ----
 
